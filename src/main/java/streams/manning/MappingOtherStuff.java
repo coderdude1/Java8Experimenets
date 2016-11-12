7f35af810f9bf5bb618e416c04624ed903d77c04
Randy <drbugsmn@gmail.com>
2016-11-11 19:10:57 -0600

    code cleanup

---
+++ src/main/java/streams/manning/MappingOtherStuff.java
@@ -0,0 +1,15 @@
+package streams.manning;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Randy on 11/10/2016.
+ */
+public class MappingOtherStuff {
+    private static final Logger LOG = LoggerFactory.getLogger(MappingOtherStuff.class);
+
+    public static void main(String[] args) {
+
+    }
+}
