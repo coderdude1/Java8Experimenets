ed00ac33ac04275e66953929f8cad7accaff8a66
rwatson <randy.watson@merrillcorp.com>
2016-11-15 14:44:43 -0600

    added 5.4 stuff for findFirst, findAny and others

--- src/main/java/streams/manning/MoreAdvancedFiltering.java
+++ src/main/java/streams/manning/MoreAdvancedFiltering.java
@@ -1,4 +1,89 @@
 package streams.manning;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
 public class MoreAdvancedFiltering {
+    private static final Logger LOG = LoggerFactory.getLogger(MoreAdvancedFiltering.class);
+
+    public static void main(String[] args) {
+        MoreAdvancedFiltering moreAdvancedFiltering = new MoreAdvancedFiltering();
+        moreAdvancedFiltering.anyMatchDemo();
+        moreAdvancedFiltering.allMatchDemo();
+        moreAdvancedFiltering.noneMatchDemo();
+        moreAdvancedFiltering.findAnyDemo();
+        moreAdvancedFiltering.findOneDemo();
+    }
+
+    private void anyMatchDemo() {
+        LOG.info("\n\n*****************   anyMatch demo *******************");
+
+        if(Dish.createMenu().stream().anyMatch(Dish::isVegetarian)) {
+            LOG.info("There are vegetarian dishes in the menu");
+        } else {
+            LOG.info("there are no vegetarian dishes in the menu");
+        }
+    }
+
+    private void allMatchDemo() {
+        LOG.info("\n\n*****************   allMatch demo *******************");
+        allMatchLogic(700);
+        allMatchLogic(1000);
+    }
+
+    private void allMatchLogic(int calorieLimit) {
+        if(Dish.createMenu().stream().allMatch(dish -> {
+            return dish.getCalories() < calorieLimit;
+        })) {
+            LOG.info("all dishes less than {} cals", calorieLimit);
+        } else {
+            LOG.info("some dishes great than {} cals", calorieLimit);
+        }
+    }
+
+    private void noneMatchDemo() {//this is the opposite of allMatch
+        LOG.info("\n\n*****************   noneMatch demo *******************");
+        noneMatchLogic(1000);
+        noneMatchLogic(500);
+    }
+
+    private void noneMatchLogic(int calories) {
+        if(Dish.createMenu().stream()
+                .noneMatch(dish ->
+                    dish.getCalories() >= calories)) {
+            LOG.info("no dishes have more than {} calories", calories);
+        } else {
+            LOG.info("some dishes are greater than {} calories", calories);
+        }
+    }
+
+    private void findAnyDemo() {
+        LOG.info("\n\n*****************   findAny demo *******************");
+
+        Dish.createMenu().parallelStream()//note with parallel stream findAny returns a differnt value than sequential stream
+                .filter(Dish::isVegetarian)
+                .findAny()  //grab the first one and return an Optional<Dish>
+                .ifPresent(dish ->LOG.info("Found a vegetarian dish: {}", dish.getName())); //notice it is IF not IS\
+
+        Dish.createMenu().stream()//note with parallel stream findAny returns a differnt value than sequential stream
+                .filter(Dish::isVegetarian)
+                .findAny()  //grab the first one and return an Optional<Dish>
+                .ifPresent(dish ->LOG.info("Found a vegetarian dish: {}", dish.getName())); //notice it is IF not IS\
+    }
+
+    private void findOneDemo() {
+        LOG.info("\n\n*****************   findOne demo *******************");
+        Dish.createMenu().parallelStream()//note with parallel stream findFirst returns a same value than sequential stream
+                .filter(Dish::isVegetarian)
+                .findFirst()  //grab the first one and return an Optional<Dish>
+                .ifPresent(dish ->LOG.info("Found a vegetarian dish: {}", dish.getName())); //notice it is IF not IS\
+
+        Dish.createMenu().stream()//note with parallel stream findFirst returns a same value than sequential stream
+                .filter(Dish::isVegetarian)
+                .findFirst()  //grab the first one and return an Optional<Dish>
+                .ifPresent(dish ->LOG.info("Found a vegetarian dish: {}", dish.getName())); //notice it is IF not IS\
+    }
+
 }
--- src/main/java/streams/manning/README.md
+++ src/main/java/streams/manning/README.md
@@ -4,9 +4,33 @@
 use .filter and pass it a predicate lambda or method reference.  There are other types of filtering such as 
 .distinct, .limit, .skip
 
+## anyMatch, allMatch, noneMatch, findFirst and findAny
+these are specialized filters that take a lambda predicate.  These 3 operations perform short-circuiting, which means
+that it does not need to process the whole stream to return a result, ie the first occrance that violates the predicate
+will cause the stream process to stop
+
+## findFirst vs findAny
+according to the API docs:
+
+stream.findAny
+> The behavior of this operation is explicitly nondeterministic; it is free to select any element in the stream. 
+> This is to allow for maximal performance in parallel operations; the cost is that multiple invocations on the 
+> same source may not return the same result. (If a stable result is desired, use findFirst() instead.
+
+tldr; if you don't care which element is returned, use findAny, esp if you are using parallel streams as findAny is 
+not guaranteed to return the first match, whereas findFirst will (but may be slower).  findFirst works better when streams
+have encounter order, ie generated from a source that ensures order like a list.
+
+## Distinct
 From the [javadoc on distinct](http://docs.oracle.com/javase/8/docs/api/java/util/stream/Stream.html#distinct--) 
 > API Note:
-  Preserving stability for distinct() in parallel pipelines is relatively expensive (requires that the operation act as a full barrier, with substantial buffering overhead), and stability is often not needed. Using an unordered stream source (such as generate(Supplier)) or removing the ordering constraint with BaseStream.unordered() may result in significantly more efficient execution for distinct() in parallel pipelines, if the semantics of your situation permit. If consistency with encounter order is required, and you are experiencing poor performance or memory utilization with distinct() in parallel pipelines, switching to sequential execution with BaseStream.sequential() may improve performance.
+  Preserving stability for distinct() in parallel pipelines is relatively expensive (requires that the operation
+  act as a full barrier, with substantial buffering overhead), and stability is often not needed. Using an 
+  unordered stream source (such as generate(Supplier)) or removing the ordering constraint with 
+  BaseStream.unordered() may result in significantly more efficient execution for distinct() in parallel pipelines,
+  if the semantics of your situation permit. If consistency with encounter order is required, and you are 
+  experiencing poor performance or memory utilization with distinct() in parallel pipelines, switching to 
+  sequential execution with BaseStream.sequential() may improve performance.
 
 # mapping
 the .map method allows you to select inforomation from the objects.  This is similar to the select statement in
@@ -18,5 +42,7 @@ lambda or a method reference.  Note that the stream output of the .map (and .fla
 ```
 would return a stream<whatever type getName returns>
 
+# flatMap
+used to coallesce multiple streams into a single stream
 
 
